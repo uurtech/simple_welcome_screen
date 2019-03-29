5ec00eecd7981f72046e9fe3d63bd1cef39f98d4
uurtech <developer.ugur@gmail.com>
2019-03-29 06:58:05 +0300

    facebook button

--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,67 @@ class MyHomePage extends StatelessWidget {
                 height: 60.0,
                 width: 60.0,
                 decoration: new BoxDecoration(
-                  borderRadius: new BorderRadius.circular(50.0),
-                  color: Colors.green
-                ),
-                child: new Icon(Icons.local_offer,color: Colors.white),
+                    borderRadius: new BorderRadius.circular(50.0),
+                    color: Colors.green),
+                child: new Icon(Icons.local_offer, color: Colors.white),
               )
             ]),
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children:<Widget>[
+              children: <Widget>[
                 Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: new Text("Quick Bee",style: new TextStyle(fontSize: 30.0),),
+                  child: new Text(
+                    "Quick Bee",
+                    style: new TextStyle(fontSize: 30.0),
+                  ),
                 )
               ],
-            )
+            ),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: new Container(
+                      height: 70.0,
+                      width: 70.0,
+                      alignment: Alignment.center,
+                      decoration: new BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text(
+                        "Sign in With Email",
+                        style:
+                            new TextStyle(fontSize: 20.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: new Container(
+                      height: 70.0,
+                      width: 70.0,
+                      alignment: Alignment.center,
+                      decoration: new BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text(
+                        "Sign in With Facebook",
+                        style:
+                            new TextStyle(fontSize: 20.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
