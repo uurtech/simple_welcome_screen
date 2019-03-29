0d632865415e9aaa287e8c643f806dafbe7e439b
uurtech <developer.ugur@gmail.com>
2019-03-29 06:40:51 +0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Quick Bee",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: "Roboto"),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+
+}
